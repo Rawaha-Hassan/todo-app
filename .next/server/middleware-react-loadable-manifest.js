b9a3d49f0d42933a8c25da0a1d78b10ec5fc0949
Rawaha-Hassan <rawahahassankahoot75@gmail.com>
2025-01-22 19:20:41 +0500

    app done completely

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST='{"node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_app":{"id":3709,"files":["static/chunks/709.7e54a277a99376af.js"]},"node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_error":{"id":9916,"files":["static/chunks/916.7480434d259ecb41.js"]}}';
